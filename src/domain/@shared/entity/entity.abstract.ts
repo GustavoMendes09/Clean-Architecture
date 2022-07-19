e26d6bbfb604eddfd8b48562c26234ebba2a82d9
Gustavo Mendes <guguren3@hotmail.com>
2022-07-19 10:29:59 -0300

    finalizado clean architecture

---
+++ src/domain/@shared/entity/entity.abstract.ts
@@ -0,0 +1,11 @@
+import Notification from "../notification/notification"
+
+export default abstract class Entity {
+
+    public id: string;
+    public notification: Notification;
+
+    constructor() {
+        this.notification = new Notification();
+    }
+}
---
+++ src/infrastructure/api/presenters/customer.presenter.ts
@@ -0,0 +1,29 @@
+import { OutputListCustomerDto } from "../../../usecase/customer/list/list.customer.dto";
+import { toXML } from "jstoxml";
+
+export default class CustomerPresenter{
+    static listXML(data: OutputListCustomerDto): string {
+
+        const xmlOption = {
+            header: true,
+            indent: " ",
+            newline: "/n",
+            allowEmpty: true
+        };
+
+        return toXML({
+            customers: {
+                customer: data.customers.map((customer) => ({
+            id: customer.id,
+            name: customer.name,
+            address: {
+                street: customer.address.street,
+                city: customer.address.city,
+                number: customer.address.number,
+                zip: customer.address.zip
+                },
+            })),
+            }
+        }, xmlOption);
+    }
+}
